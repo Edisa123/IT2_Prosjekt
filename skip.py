3160f04753c1689c26e9d641108c80b3256c9cb6
Edisa123 <113352241+Edisa123@users.noreply.github.com>
2024-03-05 10:03:43 +0100

    ny del for skip.py

--- skip.py
+++ skip.py
@@ -9,3 +9,11 @@ class Skip:
 
         # laste skipets bilde og få dens rect
         self.image = pg.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # start hvert nye skip på bunden og midten av skjermen
+        self.rect.midbottom = self.skjerm_rect.midbottom
+
+    def blitme(self):
+        """tegn skipet på sitt nåværende område"""
+        self.skjerm.blit(self.image, self.rect)
